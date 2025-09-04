7ed32aa94ddccd4fed6e1f18e2c19fac2acb37c1
Tony Silvestri <tonys@slalom.com>
2025-09-04 16:13:31 -0400

    adding .env vars

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const onSubmit = async (data: FormData) => {
     setLoading(true);
     try {
-      const baseURL =  process.env.REACT_APP_API_URL || process.env.REACT_APP_API_BASE_URL;
+      const baseURL =  process.env.REACT_APP_API_BASE_URL;
       const result = await axios.post(`${baseURL}/api/hello`, {
         name: data.name
       });
